4786721287a69fd565ea5e5e3998eb4876ed9716
Charles Boutaud <cboutaud@users.noreply.github.com>
2023-06-26 17:50:29 +0200

    Update copy
    Update the text displayed

--- hot-homes/postcode-searcher/splash.js
+++ hot-homes/postcode-searcher/splash.js
@@ -6,7 +6,7 @@ async function onSubmit(event) {
 
   // Show error for invalid postcodes
   if (!isPostcode(search)) {
-    response.innerHTML = "We couldn\'t find this postcode"
+    response.innerHTML = "We couldn\’t find this postcode"
     return
   }
 
@@ -14,10 +14,10 @@ async function onSubmit(event) {
   // const isInArea = isPostcodeInArea(search)
   isPostcodeInArea(search).then(function (result) {
     if (result === true) {
-      response.innerHTML = "Great! You live in the area we are looking at <span style='font-size: 2em;'>&#9787;</span> Want to get involved?</br><a href='https://docs.google.com/forms/d/1J-yO0mbrfGjjEW8Hv7ni1neI7XLnaoB-3UGlL9yHR-w/viewform?edit_requested=true' target='_blank' style='text-decoration: none;'><span class='response emphasise'>&#9998;</span></a> <a class='response' href='https://docs.google.com/forms/d/1J-yO0mbrfGjjEW8Hv7ni1neI7XLnaoB-3UGlL9yHR-w/viewform?edit_requested=true' target='_blank' style='text-decoration-color: #8095C2;'>Fill out this questionnaire to register your interest</a>.</br><span class='response emphasise'>&#9728;</span> The project starts in mid July, so get in touch now!</br><a class='response' href='https://www.thebureauinvestigates.com/blog/2023-06-09/hot-homes' target='_blank' style='text-decoration: none;'><span class='response emphasise'>&#10154;</span></a> <a class='response' href='https://www.thebureauinvestigates.com/blog/2023-06-09/hot-homes' target='_blank' style='text-decoration-color: #8095C2;'>Read about the project</a>."
+      response.innerHTML = "Great! You live in the area we’re looking at <span style='font-size: 2em;'>&#9787;</span></br></br>What’s next?</br><a href='https://docs.google.com/forms/d/1J-yO0mbrfGjjEW8Hv7ni1neI7XLnaoB-3UGlL9yHR-w/viewform?edit_requested=true' target='_blank' style='text-decoration: none;'><span class='response emphasise'>&#9998;</span></a> <a class='response' href='https://docs.google.com/forms/d/1J-yO0mbrfGjjEW8Hv7ni1neI7XLnaoB-3UGlL9yHR-w/viewform?edit_requested=true' target='_blank' style='text-decoration-color: #8095C2;'>Fill out this questionnaire to register your interest</a>.</br><span class='response emphasise'>&#9728;</span> The project starts in mid July, so get in touch now!</br><a class='response' href='https://www.thebureauinvestigates.com/blog/2023-06-09/hot-homes' target='_blank' style='text-decoration: none;'><span class='response emphasise'>&#10154;</span></a> <a class='response' href='https://www.thebureauinvestigates.com/blog/2023-06-09/hot-homes' target='_blank' style='text-decoration-color: #8095C2;'>Read more about what taking part involves</a>."
       response.scrollIntoView({behavior: "smooth", block: "end"});
     } else {
-      response.innerHTML = "Unfortunately, your home falls outside the area we are looking at.</br> <a href='https://docs.google.com/document/d/1KEg2c9A3z-kThUFHD2hyOEfVqZxHGwjMdH5FvXfqZ5c' target='_blank' style='text-decoration: none;'><span class='response emphasise'>&#10154;</span></a> <a class='response' href='https://www.thebureauinvestigates.com/blog/2023-06-09/hot-homes' target='_blank' style='text-decoration-color: #8095C2;'>Read about the project</a>."
+      response.innerHTML = "Unfortunately, your home falls outside the area.</br> <a href='https://docs.google.com/document/d/1KEg2c9A3z-kThUFHD2hyOEfVqZxHGwjMdH5FvXfqZ5c' target='_blank' style='text-decoration: none;'><span class='response emphasise'>&#10154;</span></a> <a class='response' href='https://www.thebureauinvestigates.com/blog/2023-06-09/hot-homes' target='_blank' style='text-decoration-color: #8095C2;'>Still interested? Read more about the Hot Homes project</a>."
       response.scrollIntoView({behavior: "smooth", block: "end"});
     }
   })
